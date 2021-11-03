878ef82c9f4a58075c5dbe572526dac527230b89
Ethan Tweitmann <ethantweitmann@gmail.com>
2021-11-02 20:40:41 -0700

    Figuring out how to display error messages

--- main.js
+++ main.js
@@ -6,29 +6,34 @@ var invalidKeys = ['-', '+', 'e', 'E'];
 var studyButton = document.querySelector('.study-button');
 var meditateButton = document.querySelector('.meditate-button');
 var exerciseButton = document.querySelector('.exercise-button');
-var buttonImages = document.querySelectorAll('.category-button-icons')
+var buttonImages = document.querySelectorAll('.category-button-icons');
+var startActivityButton = document.querySelector('.start-activity-button');
 //inputs
 var accomplishInput = document.querySelector('.accomplish-input');
 var minutesInput = document.querySelector('.minutes-input');
 var secondsInput = document.querySelector('.seconds-input');
+var inputs = document.querySelectorAll('.accomplish-input', '.minutes-input', '.seconds-input' );
 
 //function that adds class with new style
 
 studyButton.addEventListener('click', function(event) {
-  hightlightButton(event)
+  hightlightButton(event);
 });
 
 meditateButton.addEventListener('click', function(event) {
-  hightlightButton(event)
+  hightlightButton(event);
 });
 
 exerciseButton.addEventListener('click', function(event) {
-  hightlightButton(event)
+  hightlightButton(event);
+});
+
+startActivityButton.addEventListener('click', function(event) {
+  displayErrorMessage(event);
 });
 
 minutesInput.addEventListener('keydown', preventEInput)
 secondsInput.addEventListener('keydown', preventEInput);
-
 
 
 function hightlightButton(event) {
@@ -44,9 +49,23 @@ function hightlightButton(event) {
   }
 }
 
-
 function preventEInput(event) {
   if (invalidKeys.includes(event.key)) {
     event.preventDefault();
+  }
+}
+
+//gather a list of input values
+//go through the list:
+  //if input is empty
+  --> //return error message for specific inputs
+      //
+
+function displayErrorMessage(event) {
+  event.preventDefault();
+  for (var i = 0; i < inputs.length; i++) {
+    if (!inputs[i].value) {
+      getErrorMessage(i);
+    }
   }
 }
